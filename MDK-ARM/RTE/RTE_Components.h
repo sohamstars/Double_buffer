56bf1d8fa7408b1a0ec235f9f50987a339d5b42a
unknown <sohamstars@gmail.com>
2017-02-01 21:09:33 -0700

    original project with no changes having ADC channels tied to a DMA

---
+++ MDK-ARM/RTE/RTE_Components.h
@@ -0,0 +1,14 @@
+
+/*
+ * Auto generated Run-Time-Environment Component Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'doub_buffering' 
+ * Target:  'doub_buffering' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+#endif /* RTE_COMPONENTS_H */
---
+++ MDK-ARM/doub_buffering/startup_stm32f407xx.d
@@ -0,0 +1,1 @@
+doub_buffering\startup_stm32f407xx.o: startup_stm32f407xx.s
